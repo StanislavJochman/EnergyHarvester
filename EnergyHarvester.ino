a515180076e0f5945ce6310ab64f4fe0b09deb13
HeronickSK <george@generation.sk>
2019-03-20 13:59:02 +0100

    functions

--- EnergyHarvester.ino
+++ EnergyHarvester.ino
@@ -1,10 +1,10 @@
-#define INA1 6
-#define INA2 5
-#define ENA 4
+#define INA1 11
+#define INA2 3
+#define ENA 2
 //----------------
-#define INB1 11
-#define INB2 3
-#define ENB 2
+#define INB1 6
+#define INB2 5
+#define ENB 4
 //----------------
 #define ON_OPT 8
 #define ON_GYRO 9
@@ -13,7 +13,8 @@
 //----------------
 #define SL A7
 #define SR A6
-
+//----------------
+#define led 7
 void setup() {
   Serial.begin(9600);
   pinMode(INA1,OUTPUT);
@@ -24,27 +25,93 @@ void setup() {
   pinMode(ENB,OUTPUT);
   pinMode(ON_OPT,OUTPUT);
   pinMode(ON_GYRO,OUTPUT);
-
+  pinMode(led,OUTPUT);
+  //----------------
   pinMode(INT_G,INPUT);
+  //----------------  
+  digitalWrite(ON_OPT,HIGH);
 }
 
 void loop() {
   Serial.print(analogRead(SL));
   Serial.print("  ");
   Serial.println(analogRead(SR));
-  digitalWrite(ON_OPT,HIGH);
-  digitalWrite(ENA,HIGH);
-  digitalWrite(INA1,LOW);
-  digitalWrite(INA2,HIGH);
-  digitalWrite(ENB,HIGH);
-  digitalWrite(INB1,LOW);
-  digitalWrite(INB2,HIGH);
-  delay(1000);
-  digitalWrite(ENA,HIGH);
-  digitalWrite(INA1,HIGH);
-  digitalWrite(INA2,LOW);
-  digitalWrite(ENB,HIGH);
-  digitalWrite(INB1,HIGH);
-  digitalWrite(INB2,LOW);
-  delay(1000);
+  
+  RunMotor("AB",255);
+  blink_led(50);
+}
+void RunMotor (String motor_select,int speed) {
+  if (motor_select == "A") {
+    if (speed > 0) {
+      digitalWrite(ENA,HIGH);
+      digitalWrite(INA2,LOW);
+      analogWrite(INA1,speed);
+    }
+    else if (speed < 0) {
+      digitalWrite(ENA,HIGH);
+      digitalWrite(INA1,LOW);
+      analogWrite(INA2,abs(speed));
+    }
+    else {
+      digitalWrite(ENA,LOW);
+      digitalWrite(INA1,LOW);
+      analogWrite(INA2,LOW);  
+    }     
+  }
+  
+
+  
+  else if (motor_select == "B") {
+    if (speed > 0) {
+      digitalWrite(ENB,HIGH);
+      digitalWrite(INB2,LOW);
+      analogWrite(INB1,speed);
+    }
+    else if (speed < 0) {
+      digitalWrite(ENB,HIGH);
+      digitalWrite(INB1,LOW);
+      analogWrite(INB2,abs(speed));
+    }
+    else {
+      digitalWrite(ENB,LOW);
+      digitalWrite(INB1,LOW);
+      analogWrite(INB2,LOW);  
+    }
+  }
+
+  
+  else {
+    if (speed > 0) {
+      digitalWrite(ENA,HIGH);
+      digitalWrite(ENB,HIGH);
+      digitalWrite(INA2,LOW);
+      digitalWrite(INB2,LOW);
+      analogWrite(INA1,speed);
+      analogWrite(INB1,speed);
+    }
+    else if (speed < 0) {
+      digitalWrite(ENA,HIGH);
+      digitalWrite(ENB,HIGH);
+      digitalWrite(INA2,LOW);
+      digitalWrite(INB2,LOW);
+      analogWrite(INA1,abs(speed));
+      analogWrite(INB1,abs(speed));
+    }
+    else {
+      digitalWrite(ENA,LOW);
+      digitalWrite(ENB,LOW);
+      digitalWrite(INA1,LOW);
+      digitalWrite(INB1,LOW);
+      analogWrite(INA2,LOW);  
+      analogWrite(INB2,LOW);  
+    }
+  }
+
+
+}
+void blink_led(int time){
+  digitalWrite(led,HIGH);
+  delay(time);
+  digitalWrite(led,LOW);
+  delay(time);
 }
